7984e77b9277e33c3e45135443162170b240a64d
Owain Lansdowne <lansdowneop@cardiff.ac.uk>
2022-11-14 13:11:17 +0000

    Shows data from CSV

---
+++ src/lib/csv.js
@@ -0,0 +1,48 @@
+import fs from 'fs';
+import path from 'path';
+
+const postsDirectory = path.join(process.cwd(), 'public');
+
+function readCSVFile(organisation){
+    const file = organisation + ".csv";
+    const fullPath = path.join(postsDirectory,"/",file);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    return fileContents.split("\n");
+}
+
+function readEnergyData(content){
+  let monthlyUsage = 0,monthlyCost = 0,weeklyUsage = 0,weeklyCost = 0;
+  // Iterates through the last month and weeks data
+  let index = content.length - 1488;
+  while (index < (content.length-1)){
+      // Retrieves the energy cost and energy data
+      let energyData = content[index].split(",");
+      let costData = content[(index-48)].split(",");
+      let energyUsage = parseFloat(energyData[8]);
+      // Calculates the cost for the energy usage for that half hour
+      let cost = (energyUsage * (parseFloat(costData[9])/100))
+      // Adds the half hourly usage and cost to the running total
+      monthlyUsage = monthlyUsage + energyUsage;
+      monthlyCost = monthlyCost + cost;
+      // If the data is in the last week then adds the half hourly usage and cost to the running total
+      if (index > (content.length -336)){
+          weeklyCost = weeklyCost + cost;
+          weeklyUsage = weeklyUsage + energyUsage;
+      }
+      index = index + 1;
+  }
+  // Returns the cost, energy usage, carbon emission and target comparsion from the last month and week
+  let monthlyData = {"energyUsage":parseInt(monthlyUsage),"energyCost":parseInt(monthlyCost),"carbonEmissions":"40 tCO2e","comparsionToEnergyTarget":10,"comparsionToCostTarget":-10,"comparsionToCarbonTarget":0} 
+  let weeklyData = {"energyUsage":parseInt(weeklyUsage),"energyCost":parseInt(weeklyCost),"carbonEmissions":"10 tCO2e","comparsionToEnergyTarget":10,"comparsionToCostTarget":-10,"comparsionToCarbonTarget":0} 
+  return [monthlyData,weeklyData];
+}
+
+
+
+export function calculateEnergyData(organisation) {
+
+    // Opens the data file and reads the data from within
+    let content = readCSVFile(organisation);
+    let data = readEnergyData(content);
+    return data;
+  };
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,14 +1,122 @@
 import { signOut } from "next-auth/react";
 import { getSession } from "next-auth/react";
+import arrowdown from "public/images/arrowdown.svg"
+import arrowup from "public/images/arrowup.svg"
+import reached from "public/images/reached.svg"
+import Image from 'next/image'
+import React from 'react';
+import { calculateEnergyData } from '../lib/csv';
 
-export default function Dashboard() {
+class KPIContainer extends React.Component {
+  render() {
+  return (
+    <div>
+      <h3 className="text-xl divide-y">{this.props.title}</h3>
+            <KPIData data = {this.props.data.energyUsage} targetcomparison = {this.props.data.comparsionToEnergyTarget}/>
+            <KPIData data = {this.props.data.energyCost} targetcomparison = {this.props.data.comparsionToCostTarget}/>
+            <KPIData data = {this.props.data.carbonEmissions} targetcomparison = {this.props.data.comparsionToCarbonTarget}/>
+    </div>
+    );
+  }
+}
+class KPIData extends React.Component {
+  render() {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>{this.props.data}</h1>
+        <TargetComparison value = {this.props.targetcomparison}/>
+    </div>
+    );
+  }
+}
+
+
+class TargetComparison extends React.Component {
+  render() {
+    let imageSource, text;
+    if (this.props.value >0){
+      imageSource =  arrowup;
+      text = "Above";
+    }  
+    else if (this.props.value <0){
+      imageSource =  arrowdown;
+      text = "Below";
+    }  
+    else{
+      imageSource =  reached;
+      text = "Reached";
+    }  
+      return ( 
+      <div className="flex flex-row">   
+        <Image
+          src={imageSource}
+          height={25}
+          width={25}
+          alt= {text}
+        />
+        <p>{text} your target</p>
+      </div>  
+      );
+    }
+}
+
+export default function Dashboard({data},{numberOfOptimisations}) {
   function handleSignOut() {
     signOut();
   }
 
   return (
     <div>
-      <h1>Dashboard</h1>
+      {/* <Head>
+        <title>Dashboard</title>
+      </Head> */}
+      <main>
+        <div className="flex flex-row">
+          <div className="basis-2/5 border-2 m-4 shadow">
+            <KPIContainer title="Site KPIs last week" data = {data[1]}/>    
+          </div>
+          <div className="basis-2/5 border-2 m-4 shadow">
+            <KPIContainer title="Site KPIs last month" data = {data[0]}/>    
+          </div>
+          <div className="basis-1/5 border-2 m-4 shadow">
+            <div className = "flex flex-col">
+                <h3 className="text-3xl">Pending optimisations</h3>
+              <div className="flex flex-row">
+                <div className="basis-1/3 h-full">
+                  <br/>
+                  <br/>
+                  <h3 className="text-5xl font-bold align-middle">{numberOfOptimisations}</h3>
+                </div>                
+                <div className="basis-2/3">
+                  <Image
+                    src="/images/lightbulb.svg"
+                    height={144}
+                    width={144}
+                    alt="Optimisations"
+                  />
+                </div>  
+              </div>
+              <div>
+                <div className='bg-cyan-200	 m-1 flex flex-row'>
+                  <div className = "basis-1/4">
+                    <br/>
+                    <Image
+                      src="/images/info.svg"
+                      height={49}
+                      width={49}
+                      alt="Optimisations"
+                    />
+                  </div>
+                  <div className='basis-3/4'>
+                    <p className='text-xs	'>The earlier you action your optimisations, the more effective they will be.
+  Click here to visit your Optimisations tab now.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
       <div className="flex justify-center">
         <button
           className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 bg-gray-300"
@@ -41,9 +149,9 @@ export async function getServerSideProps({ req }) {
       },
     };
   }
-
+  const data = calculateEnergyData("NHS")
   // If admin or manager, show page
   return {
-    props: { session },
+    props: { session , data}
   };
 }
