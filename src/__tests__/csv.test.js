fd58d5243b7b2eac39f9c79e0cfef74f7ea25786
Owain Lansdowne <lansdowneop@cardiff.ac.uk>
2022-11-15 16:34:50 +0000

    Complete

---
+++ src/__tests__/csv.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { calculateEnergyData } from '../lib/csv';
+
+test('Monthly Data', () => {
+    let data = calculateEnergyData("1");
+    let monthly = data[0];
+    expect(monthly.carbonEmissions).toEqual("40");
+    expect(monthly.energyCost).toEqual(46597);
+    expect(monthly.energyUsage).toEqual(3970);
+})
+test('Weekly Data', () => {
+    let data = calculateEnergyData("1");
+    let monthly = data[1];
+    expect(monthly.carbonEmissions).toEqual("10");
+    expect(monthly.energyCost).toEqual(10889);
+    expect(monthly.energyUsage).toEqual(889);
+})
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -7,6 +7,8 @@ import Image from 'next/image'
 import React from 'react';
 import { calculateEnergyData } from '../lib/csv';
 
+
+// Container that contains KPI Data
 class KPIContainer extends React.Component {
   render() {
   return (
@@ -14,7 +16,7 @@ class KPIContainer extends React.Component {
       <h3 className="text-xl divide-y text-left">{this.props.title}</h3>
             <RedLine />
             <br/>
-            <KPIData data = {this.props.data.energyUsage} targetcomparison = {this.props.target[2]} units = " mwh"/>
+            <KPIData data = {this.props.data.energyUsage} targetcomparison = {this.props.target[2]} units = " MW"/>
             <br/>
             <KPIData data = {this.props.data.energyCost} targetcomparison = {this.props.target[1]} units = "£"/>
             <br/>
@@ -24,6 +26,7 @@ class KPIContainer extends React.Component {
   }
 }
 
+// Red Line for Styling
 class RedLine extends React.Component{
   render (){
   return (
@@ -39,12 +42,16 @@ class RedLine extends React.Component{
   }
 }
 
+// Specific KPI data and the comparison to targets
 class KPIData extends React.Component {
   render() {
     let imageSource, text;
-    let comparsion = parseInt(this.props.targetcomparison) - parseInt(this.props.data);
-    let positiveValue = Math.abs( comparsion );
+    // Compares the target and the data
+    let comparison = parseInt(this.props.targetcomparison) - parseInt(this.props.data);
+    // Converts the comparison to a positive number
+    let positiveValue = Math.abs( comparison );
     let value, kpiData;
+    // Amends the unit on to the data
     if (this.props.units == "£"){
       value = "£" + positiveValue;
       kpiData = "£" + this.props.data;
@@ -52,14 +59,19 @@ class KPIData extends React.Component {
       value = positiveValue + this.props.units;
       kpiData = this.props.data + this.props.units;
     }
-    if (comparsion <0){
+
+    // Compares whether above or below their target and set image and text accordingly
+    // Above their target
+    if (comparison <0){
       imageSource =  arrowup;
       text = value + " above";
     }  
-    else if (comparsion >0){
+    // Below their target
+    else if (comparison >0){
       imageSource =  arrowdown;
       text = value + " below";
-    }  
+    } 
+    // Reached their target 
     else{
       imageSource =  reached;
       text = "Reached";
@@ -90,12 +102,14 @@ export default function Dashboard({data}) {
     <div>
       <main>
         <div className="flex flex-row">
+{/* KPI Data section */}
           <div className="basis-2/5 border-2 m-4 shadow">
             <KPIContainer title="Site KPIs last week" data = {data[1]} target = {data[3]}/>    
           </div>
           <div className="basis-2/5 border-2 m-4 shadow">
             <KPIContainer title="Site KPIs last month" data = {data[0]} target = {data[4]}/>    
           </div>
+{/* Optimisations section */}
           <div className="basis-1/5 border-2 m-4 shadow">
             <div className = "flex flex-col">
                 <h3 className="text-xl text-left">Pending optimisations</h3>
@@ -126,7 +140,7 @@ export default function Dashboard({data}) {
                       alt="Optimisations"
                     />
                   </div>
-                  <div className='basis-3/4'>
+   <div className='basis-3/4'>
                     <p className='text-xs	'>The earlier you action your optimisations, the more effective they will be.
   Click here to visit your Optimisations tab now.</p>
                   </div>
@@ -170,50 +184,69 @@ export async function getServerSideProps({ req }) {
       },
     };
   }
-// Reads number of optimisations in the database
-  let number = 0;
-  try {
-    const optimisations = await prisma.optimisations.findMany({
-      where: {
-        organisation: organisationID,
-        achived : false
-      }
-    });
-    number = optimisations.length;
-  } catch (error) {
-      console.log(error);
-  };
-  // Reads the weekly and monthly targets for energy, cost and carbon for the organisation from the database
-  let targets, weekly = [0,0,0], monthly = [0,0,0];
-  try {
-    targets = await prisma.targets.findMany(  {   
-      take: 6,
-      where: {
-        organisation: organisationID,
-      },  
-      orderBy: [
-        {
-          name: 'asc',
-        },
-        {
-          timeframe: 'desc',
-        },
-        ],
-      });
-  // Splits the target data up into weekly and monthly targets
-  weekly = [targets[0].value,targets[2].value,targets[4].value]
-  monthly = [targets[1].value,targets[3].value,targets[5].value]
-  } catch (error) {
-      console.log(error);
-  };
+  // Reads optimisations from the database
+  let number = await readOptimisations(organisationID);
   // Reads the energy data from the CSV File
   const data = calculateEnergyData(organisationID);
   // Sends all the data to the page
   data[2] = number;
-  data[3] = weekly;
-  data[4] = monthly;
+  // Reads the weekly and monthly targets for energy, cost and carbon for the organisation from the database
+  let targetData=await readTargets(organisationID);
+  data[3] = targetData[0];
+  data[4] = targetData[1];
   // If admin or manager, show page
   return {
     props: {session , data}
   };
 }
+
+// Function to read the optimisations for the organisation
+export async function readOptimisations(organisationID){
+  // Reads number of optimisations in the database
+  let number = 0;
+    try {
+      const optimisations = await prisma.optimisations.findMany({
+        where: {
+          organisation: organisationID,
+          achived : false
+        }
+      });
+      number = optimisations.length;
+    } catch (error) {
+        console.log(error);
+    };
+  return number;
+}
+
+
+// Function to read the weekly and monthly targets for the organisation
+export async function readTargets(organisationID){
+    let targets, weekly = [0,0,0], monthly = [0,0,0], data = [];
+    // Reads from the database
+    try {
+      targets = await prisma.targets.findMany(  {   
+        take: 6,
+        where: {
+          organisation: organisationID,
+        },  
+        orderBy: [
+          {
+            name: 'asc',
+          },
+          {
+            timeframe: 'desc',
+          },
+          ],
+        });
+    // Splits the target data up into weekly and monthly targets
+    weekly = [targets[0].value,targets[2].value,targets[4].value]
+    monthly = [targets[1].value,targets[3].value,targets[5].value]
+    data[0] = weekly;
+    data[1] = monthly;
+    } catch (error) {
+        console.log(error);
+    };
+    // Returns the data
+    return data;
+  }
+
