aeb8e0ffa9f82481e5b11946ef9f9c1a6ec846f5
Owain Lansdowne <lansdowneop@cardiff.ac.uk>
2022-11-17 16:13:28 +0000

    Trying to send data to the database

---
+++ src/lib/database_functions.js
@@ -0,0 +1,103 @@
+// Function to read the optimisations for the organisation
+export async function updateOptimisation(optimisationID){
+    // Reads number of optimisations in the database
+    let number = 0;
+      try {
+        const optimisations = await prisma.optimisations.update({
+          where: {
+            id: optimisationID,
+          },
+          data: {
+            archive: true,
+          }
+        });
+        return optimisations;
+      } catch (error) {
+            console.debug(error);
+            return error;
+      };
+  }
+  // Function to read the weekly and monthly targets for the organisation
+export async function readTargets(organisationID){
+      let targets, weekly = [0,0,0], monthly = [0,0,0], data = [];
+      // Reads from the database
+      try {
+        targets = await prisma.targets.findMany(  {   
+          take: 6,
+          where: {
+            organisation: organisationID,
+          },  
+          orderBy: [
+            {
+              name: 'asc',
+            },
+            {
+              timeframe: 'desc',
+            },
+            ],
+          });
+      // Splits the target data up into weekly and monthly targets
+      weekly = [targets[0].value,targets[2].value,targets[4].value]
+      monthly = [targets[1].value,targets[3].value,targets[5].value]
+      data[0] = weekly;
+      data[1] = monthly;
+      } catch (error) {
+          console.log(error);
+      };
+      // Returns the data
+      return data;
+}
+// Function to read the optimisations for the organisation
+export async function readOptimisations(organisationID){
+    // Reads number of optimisations in the database
+    let number = 0;
+      try {
+        const optimisations = await prisma.optimisations.findMany({
+          where: {
+            organisation: organisationID,
+            archived : false
+          }
+        });
+        number = optimisations.length;
+      } catch (error) {
+          console.log(error);
+      };
+    return number;
+  }
+
+// Function to read the optimisations for the organisation
+export async function readUnArchivedOptimisations(organisationID){
+    // Reads unarchived optimisations in the database
+    let optimisations;
+    try {
+        optimisations = await prisma.optimisations.findMany({
+          where: {
+            organisation: organisationID,
+            archived : false
+          }
+        });
+      } catch (error) {
+          console.log(error);
+      };
+    return optimisations;
+}
+// Function to read the optimisations for the organisation
+export async function readArchivedOptimisations(organisationID){
+    // Reads unarchived optimisations in the database
+    let optimisations;  
+    try {
+        optimisations = await prisma.optimisations.findMany({
+          where: {
+            organisation: organisationID,
+            archived : true
+          }, select: {
+            id: true,
+            title: true,
+            priority: true
+          },
+        });
+      } catch (error) {
+          console.log(error);
+      };
+    return optimisations;
+}      
