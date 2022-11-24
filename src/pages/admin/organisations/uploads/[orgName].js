7f97f270d97e3587e4a0712b434a2c7bb5643692
Owain Lansdowne <lansdowneop@cardiff.ac.uk>
2022-11-24 15:19:38 +0000

    upload file

--- src/pages/admin/organisations/uploads/[orgName].js
+++ src/pages/admin/organisations/uploads/[orgName].js
@@ -3,71 +3,38 @@ import styles from "../../../../styles/Form.module.css";
 // npm install react-icons --save
 import { useState } from "react";
 // npm install formik --save
-import { useFormik } from "formik";
-import { registerEnergyDataValidate } from "../../../../lib/validate";
 import { useRouter } from "next/router";
 import { getSession } from "next-auth/react";
 import { readSites } from "../../../../lib/database_functions";
-import path from 'path';
+import axios from 'axios';
 
 export default function UploadEnergyData({data}) {
   const [selectedFile, setSelectedFile] = useState();
 	const [isFilePicked, setIsFilePicked] = useState(false);
   const router = useRouter();
   const { orgName } = router.query;
-  const formik = useFormik({
-    initialValues: {
-      file: "",
-      site: "",
-      organisation: orgName,
-    },
-    onSubmit,
-  });
-  function changeHandler(event){
-		setSelectedFile(event.target.files[0]);
-		setIsFilePicked(true);
-  }
-
-  async function onSubmit(values) {
-    // values.newfile = JSON.stringify(selectedFile);
-    alert(selectedFile);
-    // const submit = {
-    //   method: "POST",
-    //   headers: { "Content-Type": "application/json" },
-    //   body: JSON.stringify(values),
-    // };
-    // await fetch("http://localhost:3000/api/auth/uploadFile", submit).then(
-    //   (res) =>
-    //     res.json().then((data) => {
-    //       if (data) router.push("http://localhost:3000/admin/organisations");
-    //     })
-    // );
-    let fs = require('fs');
-    let mainDirectory = path.join(process.cwd(),"/..");
-    let organisation = values.organisation;
-    let site = values.site;
-    const postsDirectory = path.join(mainDirectory, 'energyData/',organisation,"/",site,"/");
-    const fullPath = path.join(postsDirectory,"/energyData.csv");
-    try {
-      if (!fs.existsSync(path.join(mainDirectory, 'energyData/',organisation))) {
-        fs.mkdirSync(path.join(mainDirectory, 'energyData/',organisation));
-      }
-      if (!fs.existsSync(path.join(mainDirectory, 'energyData/',organisation,"/",site))) {
-          fs.mkdirSync(path.join(mainDirectory, 'energyData/',organisation,"/",site));
-      }
-    } catch (err) {
-      alert(err);
-      console.error(err);
-  }
-  fs.appendFile(fullPath, selectedFile, err => {
-      if (err) {
-        console.error(err);
-      }
-  });
-
-
-  }
   const sitesOption = data.map(site => <option value={site.name}>{site.name}</option>);
+  function onFileChange (){
+    alert("File changed");
+    setSelectedFile(event.target.files[0]);
+		setIsFilePicked(true);
+  };
+
+  function onFileUpload() {
+    alert("File uploaded");
+    // Create an object of formData
+    const formData = new FormData();
+        
+    // Update the formData object
+    formData.append(
+      "myFile",
+      selectedFile,
+      selectedFile.name
+    );
+    // Request made to the backend api
+    // Send formData object
+    axios.post("/auth/uploadFile", formData);
+  };
   return (
     <div>
       <Head>
@@ -81,38 +48,36 @@ export default function UploadEnergyData({data}) {
           <p className="w-3/4 mx-auto text-gray-400">{orgName}</p>
         </div>
 
-        <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
-          <div
+        <form className="flex flex-col gap-5" onSubmit={onFileUpload}>
+          {/* <div
             className={`${styles.input_group} ${
               formik.errors.file && formik.touched.file
                 ? "border-rose-600"
                 : ""
             }`}
-          >
+          > */}
             <input
               className={styles.input_text}
               type={"file"}
               name="file"
               placeholder="Energy Data"
-              onChange={changeHandler}
-              // {...formik.getFieldProps("file")}
+              onChange={onFileChange}
             />
-          </div>
+          {/* </div> */}
 
-          <div
+          {/* <div
             className={`${styles.input_group} ${
               formik.errors.site && formik.touched.site ? "border-rose-600" : ""
             }`}
-          >
+          > */}
             <select
               className={styles.input_text}
               name="dropdown"
-              {...formik.getFieldProps("site")}
             >
               <option value="">Select A Site</option>
               {sitesOption}
             </select>
-          </div>
+          {/* </div> */}
 
           <div className="input-button">
             <button className={styles.button} type="submit">
--- src/pages/api/auth/uploadFile.js
+++ src/pages/api/auth/uploadFile.js
@@ -1,16 +1,16 @@
 import path from 'path';
 import { emitWarning } from 'process';
 export default async function handler(req, res) {
-  // console.log(req.body.newfile);
     let fs = require('fs');
     if (req.method === "POST") {
         let mainDirectory = path.join(process.cwd(),"/..");
-        let organisation = req.body.organisation;
-        let site = req.body.site;
+        let organisation = "Cardiff University";
+        let site = "Students Union";
         let file = req.body.file;
         const postsDirectory = path.join(mainDirectory, 'energyData/',organisation,"/",site,"/");
-        const fullPath = path.join(postsDirectory,"/energyData.csv");
-        const fileContents = fs.readFileSync(file, 'utf8');
+        const fullPath = path.join(postsDirectory,"/hello.csv");
+        // const fileContents = fs.readFileSync(file, 'utf8');
+        console.log("File - ", req.body.file);
         try {
             if (!fs.existsSync(path.join(mainDirectory, 'energyData/',organisation))) {
               fs.mkdirSync(path.join(mainDirectory, 'energyData/',organisation));
@@ -21,13 +21,13 @@ export default async function handler(req, res) {
           } catch (err) {
             console.error(err);
         }
-        fs.appendFile(fullPath, fileContents, err => {
+        fs.appendFile(fullPath, file, err => {
             if (err) {
               console.error(err);
             }
         });
     res.status(200).json({ message: "Modified" });
-  // } else {
-  //   res.status(500).json({ message: "HTTP method must be POST" });
-  // }
+  } else {
+    res.status(500).json({ message: "HTTP method must be POST" });
+  }
 }
