14cdc4b9d7076ebb657f5e0838e82359f9a8f62b
Owain Lansdowne <lansdowneop@cardiff.ac.uk>
2022-11-26 12:16:08 +0000

    Can upload file

--- src/pages/api/auth/uploadFile.js
+++ src/pages/api/auth/uploadFile.js
@@ -1,33 +1,49 @@
 import path from 'path';
 import { emitWarning } from 'process';
-export default async function handler(req, res) {
-    let fs = require('fs');
-    if (req.method === "POST") {
-        let mainDirectory = path.join(process.cwd(),"/..");
-        let organisation = "Cardiff University";
-        let site = "Students Union";
-        let file = req.body.file;
-        const postsDirectory = path.join(mainDirectory, 'energyData/',organisation,"/",site,"/");
-        const fullPath = path.join(postsDirectory,"/hello.csv");
-        // const fileContents = fs.readFileSync(file, 'utf8');
-        console.log("File - ", req.body.file);
-        try {
-            if (!fs.existsSync(path.join(mainDirectory, 'energyData/',organisation))) {
-              fs.mkdirSync(path.join(mainDirectory, 'energyData/',organisation));
-            }
-            if (!fs.existsSync(path.join(mainDirectory, 'energyData/',organisation,"/",site))) {
-                fs.mkdirSync(path.join(mainDirectory, 'energyData/',organisation,"/",site));
-            }
-          } catch (err) {
-            console.error(err);
-        }
-        fs.appendFile(fullPath, file, err => {
-            if (err) {
-              console.error(err);
-            }
-        });
-    res.status(200).json({ message: "Modified" });
-  } else {
-    res.status(500).json({ message: "HTTP method must be POST" });
+import {IncomingForm} from "formidable";
+import fs from "fs";
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+async function saveFile(file){
+  try {
+    let organisation = "Cardiff University";
+    let site = "Abacws";
+    const data = fs.readFileSync(file[0].filepath);
+    const mainDirectory = path.join(process.cwd(),"/..");
+    const postsDirectory = path.join(mainDirectory, 'energyData/',organisation,"/",site,"/");
+    const fullPath = path.join(postsDirectory,file[0].originalFilename);
+    try {
+      if (!fs.existsSync(path.join(mainDirectory, 'energyData/',organisation))) {
+        fs.mkdirSync(path.join(mainDirectory, 'energyData/',organisation));
+      }
+      if (!fs.existsSync(path.join(mainDirectory, 'energyData/',organisation,"/",site))) {
+          fs.mkdirSync(path.join(mainDirectory, 'energyData/',organisation,"/",site));
+      }
+    } catch (err) {
+      alert(err);
+      console.error(err);
+    }
+    fs.writeFileSync(fullPath, data);
+    await fs.unlinkSync(file[0].filepath);
+    return;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default async function handler(req,res){
+  try {
+    const form = new IncomingForm();
+    form.parse(req, async function (err, fields, files) {
+      await saveFile(files.file);
+      return res.status(201).send("");
+    });
+  } catch (error) {
+    console.log(error)
   }
 }
