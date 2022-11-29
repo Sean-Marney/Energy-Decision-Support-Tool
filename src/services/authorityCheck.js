b0334c0e2e11956ec6b305f74dc6eb678048adb9
Sean Marney <marneys@cardiff.ac.uk>
2022-11-29 13:38:30 +0000

    Bug fix where error occurs if user attempts to access a page they are unauthorised to use

--- src/services/authorityCheck.js
+++ src/services/authorityCheck.js
@@ -12,6 +12,7 @@ export default async function (session, allowedRoles) {
     });
   } catch (error) {
     return {
+      props: {},
       redirect: {
         destination: "/home",
         permanent: false,
@@ -24,6 +25,7 @@ export default async function (session, allowedRoles) {
   // Redirect the user to the homepage if they do not have authority to continue
   if (!allowedRoles.includes(user.role)) {
     return {
+      props: {},
       redirect: {
         destination: "/dashboard",
         permanent: false,
