0471cf526dd46dca06dc60f21bc07f7bf93c66c6
Oliver Hardman <hello@oliverhardman.co.uk>
2022-12-13 05:24:48 +0000

    further improvements

--- src/pages/admin/organisations/index.js
+++ src/pages/admin/organisations/index.js
@@ -4,56 +4,22 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../../../styles/Form.module.css";
 
+import Card from "../../../components/ui/Card";
+import {Button} from "../../../components/ui/Button";
+
 export default function ManageOrganisations({ getOrganisations }) {
   function handleSignOut() {
     signOut();
   }
 
   return (
-    <div>
-      <Head>
-        <title>Admin Panel</title>
-      </Head>
+    <Card className="m-8 flex-col gap-6 w-full flex">
+      <h1 className="afterline text-5xl mb-6">Admin panel</h1>
+      <Link href="organisations/register-organisation"><Button>Register new organisation</Button></Link>
+      {getOrganisations.map((organisation) => (<Link href={"admin/organisations/" + organisation.name}><Button>{organisation.name}</Button></Link>) )}
 
-      <div className="container mx-auto text-center py-20">
-        <h3 className="text-4xl font-bold">Admin Panel</h3>
+    </Card>
 
-        <div className="flex justify-center">
-          <button
-            className="mt-5 px-10 py-1 rounded-sm bg-gray-300"
-            onClick={handleSignOut}
-          >
-            Sign Out
-          </button>
-        </div>
-
-        <div className="flex justify-center">
-          <Link
-            className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-50"
-            href={"/admin/organisations/register-organisation"}
-          >
-            Register an Organisation
-          </Link>
-        </div>
-      </div>
-
-      {/* Show all organisations as dynamic links to their own page */}
-      <div className="text-xl text-center">
-        <b>Manage Organisations:</b>
-        <ul>
-          {getOrganisations.map((organisation) => (
-            <li key={organisation.id}>
-              <Link
-                className={styles.organisation}
-                href={`/admin/organisations/${organisation.orgName}`}
-              >
-                {organisation.orgName}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
   );
 }
 
